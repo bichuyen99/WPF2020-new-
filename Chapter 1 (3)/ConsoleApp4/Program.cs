168ada5743753820657b313f1bb91c1166c42926
Vo Ngoc Bich Uyen <52061019+bichuyen99@users.noreply.github.com>
2020-05-15 15:31:47 +0500

    Lecture 3
    Chapter 1

---
+++ Chapter 1 (3)/ConsoleApp4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;           // contains fundamental classes and base classes that define commonly-used value and reference data types
+using System.Windows;   // provides several important WPF base element classes
+using System.Windows.Input; // provides several important WPF base element classes
+namespace Petzold.ThrowWindowParty {
+    class ThrowWindowParty : Application {
+        [STAThread]
+        public static void Main() {
+            ThrowWindowParty app = new ThrowWindowParty();          // creates a WPF application
+            app.Run(); }                                            // starts that application
+        protected override void OnStartup(StartupEventArgs args) {
+            Window winMain = new Window();                          // creates a window
+            winMain.Title = "Main Window";                          // sets window's title
+            winMain.MouseDown += WindowOnMouseDown;                 // occurs when any mouse button is pressed while the pointer is over this element 
+            winMain.Show();                                         // opens a window and returns without waiting for the newly opened window to close
+            for (int i = 0; i < 2; i++) {                           // a loop from 0 to 2
+                Window win = new Window();                          // creates new windows
+                win.Title = "Extra Window No. " + (i + 1); win.Show(); } } // sets windows's titles
+        void WindowOnMouseDown(object sender, MouseButtonEventArgs args) {
+            Window win = new Window();                              // creates new window
+            win.Title = "Modal Dialog Box";                         // sets window's title
+            win.ShowDialog(); } } }                                 // opens a window and returns without waiting for the newly opened window to close
+
