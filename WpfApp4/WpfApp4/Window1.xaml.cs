56788739141c2040904a2fb01f6b6aeaea242d1d
Vo Ngoc Bich Uyen <52061019+bichuyen99@users.noreply.github.com>
2020-06-20 14:28:56 +0500

    Add files via upload

---
+++ WpfApp4/WpfApp4/Window1.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using WpfApp4;
+
+namespace Play
+{
+
+    public partial class Window1 : Window
+    {
+        private DispatcherTimer t = new DispatcherTimer();
+        private double time = 0;
+        double g = 9.8;
+        double angle;
+        double v0;
+        // position of bird
+        double x0;
+        double y0;
+        double a;
+        double Hmax;
+        double sumt;
+        double xmax;
+        // position of birdnest
+        double x1;          
+        double y1;
+        double thight;
+        Random r = new Random();
+
+        private void Data(out double v0, out double angle)
+        {
+            v0 = Convert.ToDouble(v0textbox.Text);
+            angle = Convert.ToDouble(angletextbox.Text);
+        }
+
+        public Window1()
+        {
+            InitializeComponent();
+            Play();
+            check();
+            result();
+         }
+        private void Play ()
+        {
+            Button play = new Button();
+        }
+        private void play (object sender, RoutedEventArgs e)
+        {
+            SoundPlayerAction sndprcs = new SoundPlayerAction();
+            sndprcs.Source = new Uri(@"slow-down-sound-effect.mp3");
+            t.Tick += new EventHandler(OnTimer);
+            t.Interval = new TimeSpan(0, 0, 0, 0, 50);
+            t.Start();
+        }
+
+        private void menu (object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Visibility = Visibility.Hidden;
+            main.playbtn.IsEnabled = false;
+            main.instrubtn.IsEnabled = false;
+            main.setbtn.IsEnabled = false;
+            SoundPlayerAction sndintro = new SoundPlayerAction();
+            sndintro.Source = new Uri(@"Birds-chirping-sound-effect.mp3");
+        }
+        void OnTimer(object sender, EventArgs e)
+        {
+              //Position of bird
+            x0 = Canvas.GetLeft(Bird) + Bird.Width / 2;
+            y0 = Canvas.GetTop(Bird) + Bird.Height / 2;
+            a = angle * Math.PI / 180;
+            // the hightest place
+            Hmax = Math.Pow(v0 * Math.Sin(a), 2) / (2 * g) + y0;
+            // time of all process
+            sumt = v0 * Math.Sin(a) / g + Math.Sqrt(2 * (y0 + Hmax) / g);
+            thight = v0 * Math.Sin(a) / g;
+            // the farthest place
+            xmax = x0 + Math.Pow(v0, 2) * Math.Sin(2 * a) / (2 * g) + v0 * Math.Cos(a) * Math.Sqrt(2 * (y0 + Hmax) / g);
+            x1 = r.Next(650, 750);
+            y1 = y0;
+
+            Polyline pl = (Polyline)this.FindName("pln");
+            Image bird = (Image)this.FindName("Bird");
+            Image nest = (Image)this.FindName("Nest");
+
+            time += 0.1;
+            if (time <= thight)
+            {
+                pl.Points.Add(new Point(x0 + v0 * Math.Cos(a) * time, y0 - v0 * Math.Sin(a) * time + 0.5 * g * Math.Sqrt(time)));
+            }
+            else if (thight < time & time <= sumt)
+            {
+                pl.Points.Add(new Point(x0 + v0 * Math.Cos(a) * time, y0 - 0.5 * g * Math.Sqrt(time)));
+            }
+            else
+            {
+                t.Stop();
+            }
+
+            Canvas.SetLeft(Bird, pl.Points.Last().X);
+            Canvas.SetTop(Bird, pl.Points.Last().Y);
+        }
+        private void check()
+        {
+            if (xmax == x1)
+            {
+                TextBlock win = new TextBlock();
+                win.Text = "YOU WIN";
+                win.MaxHeight = 150;
+                win.MaxWidth = 150;
+                win.Margin = new Thickness(280);
+                SoundPlayerAction sndwin = new SoundPlayerAction();
+                sndwin.Source = new Uri(@"Tada-sound.mp3");
+            }
+            else
+            {
+                TextBlock lose = new TextBlock();
+                lose.Text = "YOU LOSE";
+                lose.MaxHeight = 150;
+                lose.MaxWidth = 150;
+                lose.Margin = new Thickness(280);
+                SoundPlayerAction sndlos = new SoundPlayerAction();
+                sndlos.Source = new Uri(@"game-over-piano-sound-effect.mp3");
+            }
+        }
+        private void result ()
+        {
+            Button replay = new Button();
+            replay.Content = "REPLAY";
+            replay.MaxHeight = 100;
+            replay.MaxWidth = 100;
+            replay.Margin = new Thickness(280);
+        }
+        private void replay(object sender, RoutedEventArgs e)
+        {
+            time = 0;
+            t.Tick += new EventHandler(OnTimer);
+            t.Interval = new TimeSpan(0, 0, 0, 0, 50);
+            t.Start();
+            SoundPlayerAction sndprcs = new SoundPlayerAction();
+            sndprcs.Source = new Uri(@"slow-down-sound-effect.mp3");
+        }
+    }
+        
+    }
+
+
+//
+//
+//
---
+++ WpfApp4/WpfApp4/Window2.xaml.cs
@@ -0,0 +1,184 @@
+﻿using Play;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using WpfApp4;
+
+namespace Set
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+             InitializeComponent();
+         }
+            // background
+            private void btnbgr(object sender, RoutedEventArgs e)
+            {
+                Image morning = new Image();
+                Image afternoon = new Image();
+                Image defu = new Image();
+                morning.Width = 150;
+                morning.Height = 150;
+                afternoon.Width = 150;
+                afternoon.Height = 150;
+                defu.Width = 150;
+                defu.Height = 150;
+                morning.Source = new BitmapImage(new Uri(@"landscape.jpg"));
+                afternoon.Source = new BitmapImage(new Uri(@"landscape2.jpg"));
+                defu.Source = new BitmapImage(new Uri(@"dong co 3.jpg"));
+
+                DockPanel bckgrd = new DockPanel();
+                bckgrd.Margin = new Thickness(25,100,0,0);
+                bckgrd.Children.Add(morning);
+
+                DockPanel bckgrd1 = new DockPanel();
+                bckgrd1.Margin = new Thickness(25, 100, 0, 0);
+                bckgrd1.Children.Add(afternoon);
+
+                DockPanel bckgrd2 = new DockPanel();
+                bckgrd2.Margin = new Thickness(25, 100, 0, 0);
+                bckgrd2.Children.Add(defu);
+
+                Button btn = new Button();
+                btn.Content = bckgrd;
+                MyCanvas.Children.Add(btn);
+
+                Button btn1 = new Button();
+                btn1.Content = bckgrd1;
+                MyCanvas.Children.Add(btn1);
+
+                Button btn2 = new Button();
+                btn2.Content = bckgrd;
+                MyCanvas.Children.Add(btn2);
+
+            }
+            private void btn(object sender, RoutedEventArgs e)
+            {
+                ImageBrush myBrush = new ImageBrush();
+                myBrush.ImageSource = new BitmapImage(new Uri(@"landscape.jpg"));
+                MyCanvas.Background = myBrush;
+                Window1 background1 = new Window1();
+                background1.MyCanvas.Background = MyCanvas.Background;
+                MainWindow mainbckgrnd = new MainWindow();
+                mainbckgrnd.MyCanvas.Background = MyCanvas.Background;
+        }
+            private void btn1(object sender, RoutedEventArgs e)
+            {
+                ImageBrush myBrush1 = new ImageBrush();
+                myBrush1.ImageSource = new BitmapImage(new Uri(@"landscape2.jpg"));
+                MyCanvas.Background = myBrush1;
+                Window1 background1 = new Window1();
+                background1.MyCanvas.Background = MyCanvas.Background;
+                MainWindow mainbckgrnd = new MainWindow();
+                mainbckgrnd.MyCanvas.Background = MyCanvas.Background;
+        }
+            private void btn2(object sender, RoutedEventArgs e)
+            {
+                ImageBrush myBrush2 = new ImageBrush();
+                myBrush2.ImageSource = new BitmapImage(new Uri(@"dong co 3.jpg"));
+                MyCanvas.Background = myBrush2;
+                Window1 background1 = new Window1();
+                background1.MyCanvas.Background = MyCanvas.Background;
+                MainWindow mainbckgrnd = new MainWindow();
+                mainbckgrnd.MyCanvas.Background = MyCanvas.Background;
+        }
+            // Character
+            private void btnchr(object sender, RoutedEventArgs e)
+            {
+                Image redg = new Image();
+                Image redb = new Image();
+                Image yellow = new Image();
+                redg.Width = 100;
+                redg.Height = 100;
+                redb.Width = 100;
+                redb.Height = 100;
+                yellow.Width = 100;
+                yellow.Height = 100;
+                redg.Source = new BitmapImage(new Uri(@"birdg.png"));
+                redb.Source = new BitmapImage(new Uri(@"birdb.png"));
+                yellow.Source = new BitmapImage(new Uri(@"birdy.png"));
+
+                DockPanel birdgirl = new DockPanel();
+                birdgirl.Margin = new Thickness(25, 100, 0, 0);
+                birdgirl.Children.Add(redg);
+
+                DockPanel birdboy = new DockPanel();
+                birdboy.Margin = new Thickness(25, 100, 0, 0);
+                birdboy.Children.Add(redb);
+
+                DockPanel birdyell = new DockPanel();
+                birdyell.Margin = new Thickness(25, 100, 0, 0);
+                birdyell.Children.Add(yellow);
+
+                Button btng = new Button();
+                btng.Content = birdgirl;
+                MyCanvas.Children.Add(btng);
+
+                Button btnb = new Button();
+                btnb.Content = birdboy;
+                MyCanvas.Children.Add(btnb);
+
+                Button btny = new Button();
+                btny.Content = birdyell;
+                MyCanvas.Children.Add(btny);
+
+            }
+
+            private void btng(object sender, RoutedEventArgs e)
+            {
+                Window1 newwindow1 = new Window1();
+                newwindow1.Bird.Source = new BitmapImage(new Uri(@"birdg.png"));
+            }
+            private void btnb(object sender, RoutedEventArgs e)
+            {
+                Window1 newwindow1 = new Window1();
+                newwindow1.Bird.Source = new BitmapImage(new Uri(@"birdb.png"));
+            }
+            private void btny(object sender, RoutedEventArgs e)
+            {
+                 Window1 newwindow1 = new Window1();
+                 newwindow1.Bird.Source = new BitmapImage(new Uri(@"birdy.png"));
+             }
+        // Sound
+        private void btnsnd (object sender, RoutedEventArgs e)
+        {
+            Image on = new Image();
+            on.Source = new BitmapImage(new Uri(@"volume.png"));
+            Image off = new Image();
+            off.Source = new BitmapImage(new Uri(@"mute.png"));
+
+            DockPanel vol = new DockPanel();
+            vol.Margin = new Thickness(200, 150, 0, 0);
+            vol.Children.Add(on);
+
+            DockPanel mut = new DockPanel();
+            mut.Margin = new Thickness(25, 150, 0, 0);
+            mut.Children.Add(off);
+
+            Button btnvol = new Button();
+            btnvol.Content = on;
+            MyCanvas.Children.Add(btnvol);
+
+            Button btnmut = new Button();
+            btnmut.Content = off;
+            MyCanvas.Children.Add(btnmut);
+            }
+        private void btnOn (object sender, RoutedEventArgs e)
+        {
+            System.Media.SoundPlayer sdon = new System.Media.SoundPlayer();
+            sdon.Play();
+        }
+        private void btnOff(object sender, RoutedEventArgs e)
+        {
+            System.Media.SoundPlayer sdoff = new System.Media.SoundPlayer();
+            sdoff.Stop();
+        }
+    }
+    }
+
