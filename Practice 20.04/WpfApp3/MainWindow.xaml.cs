35748840e22dd0ec751209000a10aa9913c57748
justjune <justjune@live.ru>
2020-06-20 22:36:47 +0500

    Some Corrections Working Sample

--- Practice 20.04/WpfApp3/MainWindow.xaml.cs
+++ Practice 20.04/WpfApp3/MainWindow.xaml.cs
@@ -17,8 +17,8 @@ namespace WpfApp3
     {
         double g = 9.8;
         double ymin;
-        double angle;
-        double v0;
+        double angle=45;
+        double v0=80;
         double x0;
         double y0;
         double a;
@@ -42,57 +42,73 @@ namespace WpfApp3
          
         public MainWindow()
         {
-            check();
+            check();            
+            t.Tick += new EventHandler(OnTimer);
+            t.Interval = new TimeSpan(0, 0, 0, 0, 50);
         }
 
         private void play(object sender, RoutedEventArgs e)
         {
-            t.Tick += new EventHandler(OnTimer);
-            t.Interval = new TimeSpan(0, 0, 0, 0, 50);
+            v0 = Convert.ToDouble(v0textbox.Text);
+            angle = Convert.ToDouble(angletextbox.Text);
+
+            // Canvas dimensions
+            Canvas cnv = (Canvas)this.FindName("cnv");
+            double cvh = cnv.ActualHeight;
+            double cvw = cnv.ActualWidth;
+
+            //Position of bird
+            Image Bird = (Image)this.FindName("bird");
+            Image Nest = (Image)this.FindName("nest");
+
+            x0 = 120+35;
+            y0 = 480+35;
+            a = angle * Math.PI / 180;
+            // the hightest place
+            Hmax = y0 - Math.Pow(v0 * Math.Sin(a), 2) / (2 * g);
+            // time of all process
+            sumt = v0 * Math.Sin(a) / g + Math.Sqrt(2 * (y0 + Hmax) / g);
+            thight = 2 * v0 * Math.Sin(a) / g;
+            // the farthest place
+            xmax = x0 + Math.Pow(v0 * Math.Sin(2 * a), 2) / g;
+            ymin = y0;
+            x1 = r.Next(650, 750);
+            y1 = r.Next(500, 600);
+         
             t.Start();
         }
 
         void OnTimer(object sender, EventArgs e)
         {
-            //Position of bird
-            x0 = Canvas.GetLeft(bird) + bird.Width / 2;
-            y0 = Canvas.GetTop(bird) + bird.Height / 2;
-            a = angle * Math.PI / 180;
-            // the hightest place
-            Hmax = Math.Pow(v0 * Math.Sin(a), 2) / (2 * g) + y0;
-            // time of all process
-            sumt = v0 * Math.Sin(a) / g + Math.Sqrt(2 * (y0 + Hmax) / g);
-            thight = v0 * Math.Sin(a) / g;
-            // the farthest place
-            xmax = x0 + Math.Pow(v0, 2) * Math.Sin(2 * a) / (2 * g) + v0 * Math.Cos(a) * Math.Sqrt(2 * (y0 + Hmax) / g);
-            ymin = y0;
-            x1 = r.Next(650,750);
-            y1 = r.Next(500, 600);
 
-            Polyline pl = (Polyline)this.FindName("pln");
+            //Position of bird
             Image Bird = (Image)this.FindName("bird");
-            Image Nest = (Image)this.FindName("nest");
+            Polyline pl = (Polyline)this.FindName("pln");
+
 
             time += 0.1;
             if (time <= thight)
             {
-                pl.Points.Add(new Point(x0 + v0 * Math.Cos(a) * time, y0 - v0*Math.Sin(a)*time + 0.5*g*Math.Sqrt(time)));
+                pl.Points.Add(new Point(x0 + v0 * Math.Cos(a) * time, y0 - v0*Math.Sin(a)*time + 0.5*g*time*time));
+
+                Canvas.SetLeft(Bird, pl.Points.Last().X - 35);
+                Canvas.SetTop(Bird, pl.Points.Last().Y - 35);
             }
-            else if (thight < time & time <= sumt)
+           /* else if (thight < time & time <= sumt)
             {
-                pl.Points.Add(new Point(x0 + v0 * Math.Cos(a) * time, y0 - 0.5* g * Math.Sqrt(time)));
-            }
+                pl.Points.Add(new Point(x0 + v0 * Math.Cos(a) * time, y0 - 0.5* g * time*time));
+            }*/
             else
             {
                 t.Stop();
+                check();
+                pl.Points.Clear();
             }
 
-            Canvas.SetLeft(Bird, pl.Points.Last().X);
-            Canvas.SetTop(Bird, pl.Points.Last().Y);
          }
         private void check()
         {
-            if (xmax == x1 & ymin == y1)
+            if (Math.Abs(xmax - x1)<=70 && Math.Abs(ymin - y1)<=70)
             {
                 TextBlock win = new TextBlock();
                 win.Text = "YOU WIN";
