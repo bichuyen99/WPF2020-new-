0fef2e27ca532087d883ed46e14da525aee82815
Vo Ngoc Bich Uyen <52061019+bichuyen99@users.noreply.github.com>
2020-06-20 14:50:31 +0500

    Add files via upload

--- WpfApp4/WpfApp4/AssemblyInfo.cs
+++ WpfApp4/WpfApp4/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WpfApp3")]
+[assembly: AssemblyTitle("WpfApp4")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Hewlett-Packard")]
-[assembly: AssemblyProduct("WpfApp3")]
+[assembly: AssemblyProduct("WpfApp4")]
 [assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
